827ac0364cd21610fdf578bf5b0d2a285f1f7535
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-15 13:50:28 -0300

    Dev: Criada a pagina de login

---
+++ src/pages/login.jsx
@@ -0,0 +1,25 @@
+export default function Login(props) {
+    return (
+        <div className="login-page">
+            <div className="login-page__container">
+                <div className="login-page__container__logo">
+                    <img src={logo} alt="logo" />
+                </div>
+                <div className="login-page__container__form">
+                    <div className="login-page__container__form__title">
+                        <h1>Login</h1>
+                    </div>
+                    <div className="login-page__container__form__input">
+                        <input type="text" placeholder="Username" />
+                    </div>
+                    <div className="login-page__container__form__input">
+                        <input type="password" placeholder="Password" />
+                    </div>
+                    <div className="login-page__container__form__button">
+                        <button>Login</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
