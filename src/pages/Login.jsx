e8952647626f796f2a1330650888052685bc2fbe
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-15 15:05:52 -0300

    Dev: Finaliza o login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,77 @@
 import logo from './styles/img/xp-inc-gray-white.png';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
 export default function Login(props) {
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+  
+    const buttonDisabled = () => {
+        const minPasswordLength = 6;   
+        const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return !emailRegex.test(email) || password.length < minPasswordLength;
+    }
+
+    const handleLogin = (e) => {
+    e.preventDefault();
+    const { history } = props;
+    history.push('/painel');
+    }
     return (
-        <div className="login-page">
-            <div className="login-page__container">
-                <div className="login-page__container__logo">
-                    <img src={logo} alt="logo" />
-                </div>
-                <div className="login-page__container__form">
-                    <div className="login-page__container__form__title">
-                        <h1>Login</h1>
-                    </div>
-                    <div className="login-page__container__form__input">
-                        <input type="text" placeholder="Username" />
-                    </div>
-                    <div className="login-page__container__form__input">
-                        <input type="password" placeholder="Password" />
-                    </div>
-                    <div className="login-page__container__form__button">
-                        <button>Login</button>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Form>
+        <Form.Group 
+            className="mb-3" 
+            controlId="formBasicEmail">
+          <Form.Label>
+            Email
+          </Form.Label>
+          <Form.Control 
+            onChange={ (e) => setEmail(e.target.value) }
+            type="email" 
+            placeholder="E-mail" />
+          <Form.Text 
+            className="text-muted">
+                Nunca compartilhe suas credenciais!
+          </Form.Text>
+        </Form.Group>
+  
+        <Form.Group 
+            className="mb-3" 
+            controlId="formBasicPassword">
+          <Form.Label>
+            Senha
+          </Form.Label>
+          <Form.Control 
+            onChange={ (e) => setPassword(e.target.value) }
+            type="password" 
+            placeholder="Senha" />
+          </Form.Group>
+        <Form.Group 
+            className="mb-3" 
+            controlId="formBasicCheckbox">
+          <Form.Check 
+            type="checkbox" 
+            label="Salvar usuario" />
+        </Form.Group>
+        <Button 
+            onClick={ handleLogin }
+            disabled={buttonDisabled()}
+            variant="primary" 
+            type="submit">
+                Acessar
+        </Button>
+      </Form>
     );
 }
+
+Login.propTypes = {
+    history: PropTypes.shape({
+      push: PropTypes.func.isRequired,
+    }).isRequired,
+  };
