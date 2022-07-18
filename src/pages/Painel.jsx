a5b18cfab846c7e19982f6e1ae3edad6d6a2598e
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-18 11:34:48 -0300

    Dev: Cria o painel de investimentos

--- src/pages/Painel.jsx
+++ src/pages/Painel.jsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { useState, useEffect } from "react";
 import { useHistory, Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
@@ -175,3 +176,76 @@ const stockMarket = [
     );
 }
 // 
+=======
+import React from 'react'
+import PropTypes from 'prop-types'
+import Button from 'react-bootstrap/Button'
+import UserStock from '../component/UserStock'
+import MarketStock from '../component/MarketStock'
+import Header from '../component/Header'
+
+import { FaArrowAltCircleRight, FaArrowAltCircleDown } from 'react-icons/fa'
+
+const Painel = props => {
+
+const pushToLogout = () => {
+        props.history.push('/')
+    }
+
+
+    const pushToDeposit = () => {
+        props.history.push('/painel/investimentos/saldo');
+    }
+
+  return (
+    <>
+    <Header />
+    <div className="container">
+        <div className="row">
+            <div className="col-md-12">
+                <div className="card">
+                    <div className="card-header">
+                        <h4>Painel de investimentos</h4>
+                        {/* Profile Icon  */}
+                        <Button 
+                        onClick={pushToLogout}
+                        className="card-header-icon">
+
+                            Logout <FaArrowAltCircleDown /> 
+                        </Button>
+
+
+                    </div>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-md-12">
+                            <UserStock />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="card-body">
+                        <div className="row">
+                            <div className="col-md-12">
+                            <MarketStock />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="d-grid gap-2">
+            <Button 
+            onClick={pushToDeposit}
+            variant="secondary" size="lg">
+                Deposito/Retirada <FaArrowAltCircleRight />
+            </Button>
+    </div>
+    </div>
+    </>
+  )
+}
+
+Painel.propTypes = {}
+
+export default Painel
+>>>>>>> Stashed changes
