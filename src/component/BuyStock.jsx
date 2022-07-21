2b4f9c983e5be37161a13040cf6cd51259663e72
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-21 02:19:48 -0300

    Dev: Criado pagina para usuario comprar ações!

---
+++ src/component/BuyStock.jsx
@@ -0,0 +1,183 @@
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+import { Button, Card } from 'react-bootstrap'
+import { Table } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form'
+import { useState } from 'react';
+import Header from './Header'
+import { FaArrowAltCircleRight, FaArrowAltCircleUp } from 'react-icons/fa'
+import Alert from 'react-bootstrap/Alert';
+import { FaArrowAltCircleLeft, FaArrowAltCircleDown } from 'react-icons/fa';
+
+const BuyStock = props => {
+    const [userStocks, setUserStocks] = useState([]);
+    const [userBalance, setUserBalance] = useState(0);  
+    const [userQuantityToBuy, setUserQuantityToBuy] = useState(0);
+    const [userPriceToBuy, setUserPriceToBuy] = useState(0);
+
+    const [alert4, setAlert4] = useState(false);
+    const [alert5, setAlert5] = useState(false);
+
+    function userCloseAlert() {
+
+        alert4(false);
+        alert5(false);
+    }
+            // Get userStocks from Local Storage
+        useEffect(() => {
+            const userStocks = JSON.parse(localStorage.getItem('userStocks'));
+            if (userStocks && userStocks.length > 0) {
+                setUserStocks(userStocks);
+            }
+        }
+        , []);
+            // Get userBalance from Local Storage
+        useEffect(() => {
+            const userBalance = JSON.parse(localStorage.getItem('userBalance'));
+            if (userBalance) {
+                setUserBalance(userBalance);
+            }
+        }
+        , []);
+
+        
+        function handleUserInputQuantityToBuy(e) {
+            if (userBalance < e.target.value) {
+                setAlert4(true);
+                setTimeout(() => {
+                    setAlert4(false);
+                }
+                , 3000);
+            
+            }
+            
+            else {
+                const newUserStocks = [...userStocks];
+            const index = userStocks.findIndex(stock => stock.id === stock.id);
+            const userQuantityToBuy = e.target.value;
+            setUserQuantityToBuy(userQuantityToBuy);
+            console.log('userQuantityToBuy', userQuantityToBuy);
+            const stockValue = userQuantityToBuy * userStocks[index].value;
+            console.log('stockValue', stockValue);
+            setUserPriceToBuy(stockValue);
+            console.log(userBalance)
+            }
+        }
+
+
+        function buttonHandleBuy(e) {
+           
+
+            const { name, value } = e.target;
+            const index = userStocks.findIndex(stock => stock.id === stock.id);
+            const newUserStocks = [...userStocks];
+            console.log(userStocks[index].value);
+}
+
+
+    function renderCart () {
+        return (
+            <div>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Quantidade Total</th>
+                            <th>Valor Total </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>{userQuantityToBuy}</td>
+                            <td>{userPriceToBuy.toFixed(2)}
+                            </td>
+                        </tr>
+                    </tbody>
+                </Table>
+                
+            </div>
+                
+        )
+    }
+    return (
+       <>
+         <Header />
+         <Card>
+            <Card.Header>
+                <Card.Title>
+                <Alert variant="danger" show={alert4} onClose={userCloseAlert} dismissible>
+                        <Alert.Heading>Atenção!</Alert.Heading>
+                        <p>
+                            O saldo é insuficiente para comprar o ativo.
+                        </p>
+                    </Alert>
+                    <Alert variant="warning" show={alert5} onClose={userCloseAlert} dismissible>
+                        <Alert.Heading>Atenção!</Alert.Heading>
+                        <p>
+                            Insira um valor valido para comprar o ativo.
+                        </p>
+                    </Alert>
+
+                </Card.Title>
+                <Link 
+                style={ { color: 'black' } }
+                to="/painel/">
+                    <FaArrowAltCircleLeft />
+                </Link>
+                <h3>Comprar Ações</h3>
+            </Card.Header>
+            <Card.Body>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Quantidade</th>
+                            <th>Valor</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {userStocks.map(stock => (
+                            <tr key={stock.id}>
+                                <td>{stock.name}</td>
+                                <td>{stock.quantity}</td>
+                                <td>{stock.value.toFixed(2)}</td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </Table>
+                {userQuantityToBuy > 0 ? renderCart() : null}
+                  Insert how many stocks you want to buy
+                  {
+                        userStocks.map(stock => (
+                            <Form.Group id='input'>
+                                <Form.Label>{stock.name}</Form.Label>
+                                <Form.Control 
+                                id='input'
+                                onChange={handleUserInputQuantityToBuy}
+                                type="number" placeholder="Quantidade" />
+                            </Form.Group>
+                        ))
+                  }
+                   <Button
+                    onClick={buttonHandleBuy}
+                    variant="primary"
+                    type="submit">
+                    Comprar
+                </Button>
+
+
+               
+            </Card.Body>
+        </Card>
+
+
+
+
+
+       </>
+    
+
+    )
+}
+
+export default BuyStock
