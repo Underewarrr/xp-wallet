0d42a35c7b3f98dc4ccdeb3863a16247f7ae635f
Rafhael Oliveira <74227915+Underewarrr@users.noreply.github.com>
2022-07-24 13:45:42 -0300

    Fix: Erros de syntax com Eslint

--- src/component/UserStock.jsx
+++ src/component/UserStock.jsx
@@ -1,62 +1,53 @@
-import { React, useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
-import Table from 'react-bootstrap/Table'
-import Accordion from 'react-bootstrap/Accordion'
-import Button from 'react-bootstrap/Button'
+import { React, useState, useEffect } from 'react';
+import Table from 'react-bootstrap/Table';
+import Accordion from 'react-bootstrap/Accordion';
 
-const UserStock = props => {
+function UserStock() {
+  // useEffect to get from local storage
+  const [buyedStock, setBuyedStock] = useState([]);
+  useEffect(
+    () => {
+      const buyedStock = JSON.parse(localStorage.getItem('buyedStock'));
+      if (buyedStock) {
+        setBuyedStock(buyedStock);
+      }
+    },
+    [],
+  );
 
-    //useEffect to get from local storage
-const [buyedStock, setBuyedStock] = useState([]);
-const [showAlertSuccess, setshowAlertSuccess] = useState(false);
-
-
-    useEffect(() => {
-        const buyedStock = JSON.parse(localStorage.getItem('buyedStock'));
-        if (buyedStock) {
-          setBuyedStock(buyedStock);
-        }
-    }
-        , []);
-
-
-
-
-    return (
-      <Accordion>
+  return (
+    <Accordion>
       <Accordion.Item eventKey="0">
         <Accordion.Header>Minhas ações pessoais</Accordion.Header>
         <Accordion.Body>
-        <Table striped bordered hover variant="light">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Nome</th>
-            <th>Quantidade</th>
-            <th>Valor</th>          
-          </tr>
-        </thead>
-        <tbody>
-         
-          {buyedStock.filter(stock => stock.quantity > 0).map(stock => (
-            <tr key={stock.id}>
-                <td>{stock.id}</td>
-                <td>{stock.name}</td>
-                <td>{stock.quantity}</td>
-                <td>{stock.value}</td>
-            </tr>
-          
-          ))}
-        </tbody>
-      </Table>
+          <Table striped bordered hover variant="light">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nome</th>
+                <th>Quantidade</th>
+                <th>Valor</th>
+              </tr>
+            </thead>
+            <tbody>
+              {buyedStock.filter((stock) => stock.quantity > 0).map((stock) => (
+                <tr key={stock.id}>
+                  <td>{stock.id}</td>
+                  <td>{stock.name}</td>
+                  <td>{stock.quantity}</td>
+                  <td>{stock.value}</td>
+                </tr>
+
+              ))}
+            </tbody>
+          </Table>
         </Accordion.Body>
       </Accordion.Item>
-      </Accordion>
+    </Accordion>
 
-
-  )
+  );
 }
 
-UserStock.propTypes = {}
+UserStock.propTypes = {};
 
-export default UserStock
+export default UserStock;
--- src/pages/Painel.jsx
+++ src/pages/Painel.jsx
@@ -1,62 +1,63 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-import Button from 'react-bootstrap/Button'
-import UserStock from '../component/UserStock'
-import MarketStock from '../component/MarketStock'
-import Header from '../component/Header'
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import { FaArrowAltCircleRight, FaArrowAltCircleDown } from 'react-icons/fa';
+import UserStock from '../component/UserStock';
+import MarketStock from '../component/MarketStock';
+import Header from '../component/Header';
 
-import { FaArrowAltCircleRight, FaArrowAltCircleDown } from 'react-icons/fa'
+function Painel(props) {
+  const pushToLogout = () => {
+    props.history.push('/');
+  };
 
-const Painel = props => {
-
-const pushToLogout = () => {
-        props.history.push('/')
-    }
-
-
-    const pushToDeposit = () => {
-        props.history.push('/painel/investimentos/saldo');
-    }
+  const pushToDeposit = () => {
+    props.history.push('/painel/investimentos/saldo');
+  };
 
   return (
     <>
-    <Header />
-    <div className="container">
+      <Header />
+      <div className="container">
         <div className="row">
-            <div className="col-md-12">
-                <div className="card">
-                    <div className="card-header">
-                        <h4>Painel de investimentos</h4>
-                    </div>
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-md-12">
-                            <UserStock />
-                            </div>
-                        </div>
-                    </div>
-                    <div className="card-body">
-                        <div className="row">
-                            <div className="col-md-12">
-                            <MarketStock />
-                            </div>
-                        </div>
-                    </div>
+          <div className="col-md-12">
+            <div className="card">
+              <div className="card-header">
+                <h4>Painel de investimentos</h4>
+              </div>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-12">
+                    <UserStock />
+                  </div>
                 </div>
+              </div>
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-12">
+                    <MarketStock />
+                  </div>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
         <div className="d-grid gap-2">
-            <Button 
+          <Button
             onClick={pushToDeposit}
-            variant="outline-secondary" size="lg">
-                Deposito/Retirada <FaArrowAltCircleRight />
-            </Button>
-    </div>
-    </div>
+            variant="outline-secondary"
+            size="lg"
+          >
+            Deposito/Retirada
+            {' '}
+            <FaArrowAltCircleRight />
+          </Button>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-Painel.propTypes = {}
+Painel.propTypes = {};
 
-export default Painel
+export default Painel;
